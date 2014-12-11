4cd830789a2254a48632c0eb4847be3b690daccf
Jann Kleen <jann.kleen@freshx.de>
2014-12-11 18:10:24 +0200

    add comments

--- github-leaderboard.py
+++ github-leaderboard.py
@@ -46,27 +46,41 @@ def static_proxy(path):
 @app.route('/update')
 def update_from_gh():
     def calculate_score(labels):
+        """ Rate the labels assigned to an issue according to the scoreboard in settings
+        :param labels: labels to rate
+        :return: sum of all labels
+        """
         scores = []
         for label in labels:
             scores.append(settings.label_scores.get(label, 0))
         return sum(scores)
 
+    # Connect to GH and get the repo
     gh = login(settings.GITHUB_USERNAME, settings.GITHUB_TOKEN)
     repo = gh.repository(settings.GITHUB_REPO_OWNER, settings.GITHUB_REPO_NAME)
+
+    # Get all closed issues which have a milestone (our way to separate sprints)
     closed_issues = list(repo.iter_issues(state='closed'))
-    closed_issues = filter(lambda issue: issue.milestone is not None, closed_issues)
+    closed_issues = filter(lambda _issue: _issue.milestone is not None, closed_issues)
+
     response = {
         'issues': defaultdict(lambda: defaultdict(list)),
         'scores': defaultdict(lambda: defaultdict(lambda: 0)), }
 
     for issue in closed_issues:
         score = calculate_score([label.name for label in issue.labels])
+
+        # Add issue to issue list
         response['issues'][issue.milestone.title][issue.assignee.login].append(
             {'title': issue.title,
              'score': score,
              'closed': issue.closed_at.isoformat(),
              'url': issue.html_url, })
+
+        # Add score to user aggregate score
         response['scores'][issue.milestone.title][issue.assignee.login] += score
+
+    # Add rate limit returned from GH for debug reasons
     response['rate_limit'] = gh.ratelimit_remaining
     return json.dumps(response)
 
